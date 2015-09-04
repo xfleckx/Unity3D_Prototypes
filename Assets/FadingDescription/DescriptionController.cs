536944657bd52c6647af2f098a6ac5ed2429cb6b
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-09-04 09:41:35 +0200

    Add Text fading component

---
+++ Assets/FadingDescription/DescriptionController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DescriptionController : MonoBehaviour {
+
+    const string FADEIN = "FadeIn";
+    const string FADEOUT = "FadeOut";
+    const string VISIBLE = "IsVisible";
+
+    public Animator anim;
+
+    private bool isVisible = true;
+
+    public void FadeIn()
+    {
+        //Debug.Assert(anim != null);
+
+        if (isVisible)
+            return;
+ 
+        Debug.Log(FADEIN);
+        
+        anim.SetTrigger(FADEIN);
+
+        isVisible = true;
+
+        anim.SetBool(VISIBLE, isVisible);
+    }
+
+    public void FadeOut()
+    {
+        //Debug.Assert(anim != null);
+
+        if (!isVisible)
+            return;
+
+        Debug.Log(FADEOUT);
+        
+        anim.SetTrigger(FADEOUT);
+
+        isVisible = false;
+        
+        anim.SetBool(VISIBLE, isVisible);
+    }
+
+
+    void OnGUI()
+    {
+        if (GUILayout.Button(FADEIN))
+        {
+            FadeIn();
+        }
+
+        if (GUILayout.Button(FADEOUT))
+        {
+            FadeOut();
+        }
+    }
+}
